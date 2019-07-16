a27156c40ee345c90f3a84c4b5c907e93b10f95c
Daniel Schmidt <dsc@charlietango.dk>
2019-07-16 10:39:29 +0200

    chore: upgrade dependencies

--- tests/useScrollPercentage.test.js
+++ tests/useScrollPercentage.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from 'react-hooks-testing-library'
+import { renderHook } from '@testing-library/react-hooks'
 import { useScrollPercentage } from '../src/useScrollPercentage'
 
 it('should execute the useScrollPercentage hook', () => {
