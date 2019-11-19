59c6971882f182d1bee58fe82d4a1ccf242d290e
Daniel Schmidt <3764345+thebuilder@users.noreply.github.com>
2019-11-19 12:54:29 +0100

    feat: trigger scroll update on init (#137)

--- src/useScrollPercentage.tsx
+++ src/useScrollPercentage.tsx
@@ -18,33 +18,29 @@ export function useScrollPercentage(
   const target = entry && entry.target
 
   useEffect(() => {
+    const handleScroll = () => {
+      if (!target) return
+      const bounds = target.getBoundingClientRect()
+      const percentage = options.horizontal
+        ? calculateHorizontalPercentage(bounds, options.threshold, options.root)
+        : calculateVerticalPercentage(bounds, options.threshold, options.root)
+
+      setPercentage(percentage)
+    }
+
     if (inView) {
-      const handleScroll = () => {
-        if (!target) return
-        const bounds = target.getBoundingClientRect()
-        const percentage = options.horizontal
-          ? calculateHorizontalPercentage(
-              bounds,
-              options.threshold,
-              options.root,
-            )
-          : calculateVerticalPercentage(bounds, options.threshold, options.root)
-
-        setPercentage(percentage)
-      }
-
       const root = options.root || window
       root.addEventListener('scroll', handleScroll, { passive: true })
       root.addEventListener('resize', handleScroll)
-
-      // Trigger a scroll update, so we set the initial scroll percentage
-      handleScroll()
 
       return () => {
         root.removeEventListener('scroll', handleScroll)
         root.removeEventListener('resize', handleScroll)
       }
     }
+
+    // Trigger a scroll update, so we set the initial scroll percentage
+    handleScroll()
     return
   }, [inView, options.root, options.horizontal, options.threshold, target])
 
